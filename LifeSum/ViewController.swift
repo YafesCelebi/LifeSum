b744e1c501124a41202dffc702ae7c6a12ea5306
HMG <murat.gocmez@arksigner.com>
2022-07-03 13:14:02 +0300

    Initial Commit

---
+++ LifeSum/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LifeSum
+//
+//  Created by H. Murat GÖÇMEZ on 3.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
